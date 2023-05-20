b1abfc5cfa886678e24e2be3d7941efff5e2b188
tommy tran <tt130021@ohio.edu>
2023-05-20 12:10:54 -0400

    hi

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <tommy tran>
+ *        Date: <05/20/2023>
+ * Description: git tutorial
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hi git!" << endl;
+    return 0;
+}// main
